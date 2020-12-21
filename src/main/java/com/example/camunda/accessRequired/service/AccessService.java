57dc5ce7ad1b7485b8f713ab3dbb42e267cc226f
demotivirus <demotivirus@gmail.com>
2020-12-21 12:30:16 +0300

    Add work camunda with database

--- src/main/java/com/example/camunda/accessRequired/service/AccessService.java
+++
@@ -1,6 +0,0 @@
-package com.example.camunda.accessRequired.service;
-
-public interface AccessService {
-    Long create(Long entityId, String userName, String comment);
-    void update(Long entityId, String approver, Boolean isApproved);
-}
